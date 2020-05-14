e85e20511de5e146cf991b787cb0e7a6dd3c845a
brunovmc <bruno.vidigal.coriolano@gmail.com>
2020-05-14 00:43:23 -0300

    text

---
+++ script.js
@@ -0,0 +1,27 @@
+// document.getElementById('inputfile')
+//     .addEventListener('change', function() {
+//
+//     var fr=new FileReader();
+//     fr.onload=function(){
+//         document.getElementById('output')
+//                 .textContent=fr.result;
+//     }
+//
+//     fr.readAsText(this.files[0]);
+// })
+
+
+$(".content").load("~/post/noticia/teste1");
+
+
+// new File(100, "post/noticia/teste1");
+//
+//
+// function printFile(File) {
+//   var reader = new FileReader();
+//   reader.onload = function(evt) {
+//     document.getElementById('output')
+//             .textContent = reader.result;
+//   };
+//   reader.readAsText(File);
+// }
